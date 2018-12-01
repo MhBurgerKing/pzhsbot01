353165406fd9ed60b77e5ccf27c53a05016f1bfb
User0374124 <34395040+user0374124@users.noreply.github.com>
2018-12-01 19:20:52 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -457,3 +457,61 @@ bot.on("message", function(message) {
 
 
 }})
+
+bot.on('message', message => {
+
+    if (message.content === prefix + "test011"){
+    if (!message.guild.member(bot.user).hasPermission('SEND_MESSAGES')) return;
+
+    let tosend = [];
+    bot.guilds.forEach((guild, number) => { tosend.push(`\`${guild.name}\` | ${guild.memberCount} membres`) })
+
+
+    let pages = [];
+for (let i = 0; i < tosend.length;) {
+    if ((i + 9) > tosend.length) {
+        pages.push(tosend.slice(i, (i + 9) - ((i + 9) - tosend.length)).join("\n"));
+        break;
+    } else {
+        pages.push(tosend.slice(i, i + 9).join("\n"));
+        i += 9
+    };
+}
+    let page = 1;
+const embed = new Discord.RichEmbed()
+    .setTitle("Serveurs dans lequel je suis.")
+    .setColor('FFFFFF')
+    .setFooter(`Page: ${page}/${pages.length}`)
+    .setDescription(pages[page-1])
+message.channel.send(embed).then(msg => {
+    if (!message.guild.member(bot.user).hasPermission('ADD_REACTIONS')) return;
+        msg.react("◀").then(r => {
+            msg.react("▶")
+            
+            const backF = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
+            const ForF = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
+            
+            const back = msg.createReactionCollector(backF, { time: 180000 });
+            const For = msg.createReactionCollector(ForF, { time: 180000 });
+            
+            back.on('collect', async r => {
+                r.remove(message.author.id)
+                if (page === 1) return r.remove(message.author.id);
+                page--;
+                embed.setDescription(pages[page-1]);
+                embed.setFooter(`Page: ${page}/${pages.length}`);
+                msg.edit(embed);
+            });
+            
+            For.on('collect', async r => {
+                r.remove(message.author.id)
+                if (page === pages.length) return r.remove(message.author.id);
+                page++;
+                embed.setDescription(pages[page-1]);
+                embed.setFooter(`Page: ${page}/${pages.length}`);
+                msg.edit(embed);
+            });
+        });
+    });
+
+}})
